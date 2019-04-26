54a58a5ad8adce398500a9d5498eb00ef0695066
Christopher Trauco <chris@trau.co>
2019-04-26 11:06:58 -0400

    sec assessment complete, source keys updated to production-ready and secure

--- Final/MovieDB/scripts/MovieDB.js
+++ Final/MovieDB/scripts/MovieDB.js
@@ -1,10 +1,11 @@
 
+//Uncomment
 // var mainClick;
 // function preload(){
 //     mainClick = loadSound("/Media/audio/Dolphins Clicks-SoundBible.com-1458516263.mp3");
 // }
 async function fetchMoviesFromUserInput(searchQuery) {
-    const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=4e818663c4f334a33277fd88c377dea4&language=en-US&query=${searchQuery}&page=1&include_adult=false`
+    const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_KEY}&language=en-US&query=${searchQuery}&page=1&include_adult=false`;
     const response = await fetch(API_URL);
     const searchResults = await response.json();
     return searchResults;
@@ -32,6 +33,7 @@ async function movieSearch()  {
     }
 }
 
+    
 function renderMovieResults(movies) {
     clearMovieResults();
     /** get parent (container) element that will hold the list of movies */
@@ -42,7 +44,7 @@ function renderMovieResults(movies) {
         const movieResultDiv = document.createElement("div");
         movieResultDiv.innerHTML = `
         <div class="movie-result">
-        <img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}">
+        <img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2${movie.poster_path}">
         <div class="overlay">
         <div class="text">
             <h3>${movie.title}</h3>
